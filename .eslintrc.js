39129ab45c920e82d8663c65b835633194185087
Maryia Skachkova <96mariyask@gmail.com>
2023-04-30 23:50:49 +0300

    fead: add keyboard and DOM structure using javascript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,13 @@ module.exports = {
   extends: [
     'eslint-config-airbnb-base',
   ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   rules: {
     'no-console': 'off',
+    'import/extensions': 0,
   },
 };
